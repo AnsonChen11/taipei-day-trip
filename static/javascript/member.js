1a6da14288788cd5d9e71c68bb3687efbf9566db
AnsonChen11 <ab67325@gmail.com>
2023-01-05 18:44:42 +0800

    style: member page

--- static/javascript/member.js
+++ static/javascript/member.js
@@ -3,3 +3,9 @@ const fileUploader = document.querySelector("#file-uploader");
 fileUploader.addEventListener("change", (e) => {
    console.log(e.target.files); 
 });
+
+
+const profileHistoryOrders = document.querySelector(".profile_history_orders");
+profileHistoryOrders.addEventListener("click", function(){
+   location.href="/historyOrders";
+})
